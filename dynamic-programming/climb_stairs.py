e570618b255cf8c2ae63ad058f660d9751dff843
asharma13524 <asharma13524@gmail.com>
2021-03-02 22:01:42 -0500

    fixed arrow

--- dynamic-programming/climb_stairs.py
+++ dynamic-programming/climb_stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def __init__(self):
         self.dic = {1: 1, 2: 2}
 
-    def climbStairs(self, n: int) -> int:
+    def climbStairs(self, n: int):
         if n not in self.dic:
             self.dic[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return self.dic[n]
